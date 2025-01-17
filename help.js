8f701544a0806b38c6e5847572e0bd302d22a3df
bmartinson <brian@brianmartinson.com>
2025-01-16 17:50:51 -0800

    New Help Menu

---
+++ help.js
@@ -0,0 +1,42 @@
+// if (pr.user.login === 'philiptreering') {
+//   // do some special philip logging for right now
+//   console.log('~~ user name', pr.user.login);
+// }
+
+// ----- nodejs helper variables -----
+_cReset = '\x1b[0m';
+_cBright = '\x1b[1m';
+_cDim = '\x1b[2m';
+_cUnderscore = '\x1b[4m';
+_cBlink = '\x1b[5m';
+_cReverse = '\x1b[7m';
+_cHidden = '\x1b[8m';
+_cFgBlack = '\x1b[30m';
+_cFgRed = '\x1b[31m';
+_cFgGreen = '\x1b[32m';
+_cFgYellow = '\x1b[33m';
+_cFgBlue = '\x1b[34m';
+_cFgMagenta = '\x1b[35m';
+_cFgCyan = '\x1b[36m';
+_cFgWhite = '\x1b[37m';
+_cFgGray = '\x1b[90m';
+_cBgBlack = '\x1b[40m';
+_cBgRed = '\x1b[41m';
+_cBgGreen = '\x1b[42m';
+_cBgYellow = '\x1b[43m';
+_cBgBlue = '\x1b[44m';
+_cBgMagenta = '\x1b[45m';
+_cBgCyan = '\x1b[46m';
+_cBgWhite = '\x1b[47m';
+_cBgGray = '\x1b[100m';
+
+console.clear();
+
+console.log(`Commands:\n`);
+console.log(`  ${_cFgGreen}start${_cReset}\t(execute all)\n`);
+console.log(`  ${_cFgGreen}gather${_cReset} (gather data and generate .results_history/*.json)`);
+console.log(`  ${_cFgGreen}combine${_cReset} (re-generate combined results)`);
+console.log(`  ${_cFgGreen}chart${_cReset} (re-generate csv files)\n`);
+console.log(`  ${_cFgYellow}help${_cReset} (display this menu)`);
+
+console.log('');
